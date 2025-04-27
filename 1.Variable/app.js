d05c8eaf72a22bc76cd2e9c4f22069e5b137ca79
AnugrahaTamang <tamanganugrah59@gmail.com>
2025-04-27 21:07:56 +0545

    Variable

---
+++ 1.Variable/app.js
@@ -0,0 +1,29 @@
+var a = 12;
+console.log(a); //output: 12
+var a = 13;
+console.log(a); //output: 13
+a = 15;
+console.log(a); //output : 15
+// ----------------------------------------------------------------
+let ab = 12;
+console.log(ab); //output: 12
+// let ab = 13;  : not allowed
+ab = 1222; //allowed
+console.log(ab);
+output: 1222;
+//________________________________________________________________________
+const x = "anugraha tamang";
+console.log(x); // output: anugraha tamang
+// x = "ramit "; : not allowed
+// console.log(x);
+
+//________________________________
+let one = true;
+console.log(one); //output: true
+console.log(one == 1); //output:  true
+console.log(one === 1); //output: false
+
+let two = false;
+console.log(two); //output: false
+console.log(two == 0); //output: true
+console.log(two === 0); //output: false
---
+++ 1.Variable/readme.md
@@ -0,0 +1,9 @@
+# Variable
+
+- Variable is a container which store the different type of data type values. In Javascript there are three way to declare variable.
+
+1. var : this is the javascript old method of declaring variable.
+2. let
+3. const
+
+- Note: Good Approach to initialize variable for let and const.
